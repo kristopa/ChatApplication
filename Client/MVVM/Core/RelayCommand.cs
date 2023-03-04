57417a557c67e358f397ec2553faa1e5bd13e6e5
kristopa <kparas2@gmail.com>
2023-03-03 23:45:30 -0500

    Initial commit

---
+++ Client/MVVM/Core/RelayCommand.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Windows.Input;
+
+// Notes:
+// The question mark after the type means it can be set to null
+// For a example, int? num = null is valid since ? is after the type.
+// This is typically done for when reading in from DBs that have data
+// that is undefined
+
+namespace Client.MVVM.Core
+{
+    internal class RelayCommand : ICommand
+    {
+        private Action<object?> _execute;
+        private Func<object?, bool>? _canExecute;
+
+        public event EventHandler? CanExecuteChanged
+        {
+            add { CommandManager.RequerySuggested += value; }
+            remove { CommandManager.RequerySuggested -= value; }
+        }
+
+        public RelayCommand(Action<object?> execute, Func<object?, bool>? canExecute = null)
+        {
+            _execute = execute;
+            _canExecute = canExecute;
+        }
+
+        public bool CanExecute(object? parameter)
+        {
+            return _canExecute == null || _canExecute(parameter);
+        }
+
+        public void Execute(object? parameter)
+        {
+            _execute(parameter);
+        }
+    }
+}
---
+++ Client/MVVM/ViewModel/MainViewModel.cs
@@ -0,0 +1,17 @@
+﻿using Client.MVVM.Core;
+using Client.Net;
+
+namespace Client.MVVM.ViewModel
+{
+    internal class MainViewModel
+    {
+        public RelayCommand ConnectToServerCommand { get; set; }
+
+        private Server _server;
+        public MainViewModel()
+        {
+            _server = new Server();
+            ConnectToServerCommand = new RelayCommand(o => _server.ConnectToServer());
+        }
+    }
+}
